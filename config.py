a285cecb178c64acdc8aa4b913ac47e548505cbd
evgen <evgen19-97@ya.ru>
2018-10-04 21:14:17 +0300

    add config file

---
+++ config.py
@@ -0,0 +1,10 @@
+# Tune configuration for N, M, r,p,q there
+# for q use npunpy arrange(start,end,step)
+
+from numpy import arange
+
+N = 1024
+M = 100000
+r = 1.0
+p = [.0]
+q = arange(0.1400,0.1440,0.0002)
