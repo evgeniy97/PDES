3262584885e558fd3ccb51cbca09b9c591ea2d41
evgen <evgen19-97@ya.ru>
2018-10-06 13:05:14 +0300

    add first statistic

---
+++ statistic1.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import sys as sys # to get argument from command line
+
+# Так как мы не используем ошибку полученную на этом шаге в дальнейшем, в этой версии я ее пропустил;
+
+number_of_simulation = 1000 
+number_of_timesteps = 100000 # Unused
+def main(path):
+    """""
+    path: path to directory
+    """""
+    # Initiliaze colum names
+    file_path = path + '/test{}.txt'
+    names =['step','min','max','average','width','utilization']
+    data = pd.DataFrame()
+    for i in range(number_of_simulation):
+        data += pd.read_table(file_path.format(i),sep=' ',names=names,header=None,index_col='step')
+
+    data /= number_of_simulation
+
+    # Save data
+
+    out_file_path = path + '.txt'
+
+    data.to_csv(out_file_path, header=None, sep=' ', mode='w')
+    # read txt file as data = pd.read_table(FILE_PATH,sep=' ',names=names,header=None,index_col='step')
+
+
+
+if __name__ == "__main__":
+    main(sys.argv[1]) # sys.argv[0] is a name of the script
